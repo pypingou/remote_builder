028ce51d91eb3ef0251bc81e17261edf6f145aa1
Pierre-Yves Chibon <pingou@pingoured.fr>
2022-02-02 14:40:41 +0100

    Implement declaring the build servers via a configuration file
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- remote_builder/cli/cli.py
+++ remote_builder/cli/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import configparser
 import logging
 import os
 import sys
@@ -9,6 +10,16 @@ from remote_builder.server import exceptions
 
 
 _log = logging.getLogger(__name__)
+
+
+_default_config = """
+[hosts]
+host1 = localhost
+[localhost]
+type=       socket
+port=       18861
+timeout=    14400
+"""
 
 
 def _parser_rpmbuild(subparser):
@@ -61,6 +72,11 @@ def parse_arguments(args=None):
     )
 
     parser.add_argument(
+        "--config",
+        help="Configuration file to use",
+    )
+
+    parser.add_argument(
         "--host",
         default="localhost",
         help="Host of the run the server at (detaulf to localhost)",
@@ -82,7 +98,87 @@ def parse_arguments(args=None):
     return parser.parse_args(args)
 
 
-def do_rpmbuild(conn, args):
+def _validate_config(config):
+    """Validate the configuration file."""
+
+    if not config.has_section("hosts"):
+        raise BaseRemoteBuilderError("Configuration file has no 'hosts' section")
+
+    for _, host in config.items("hosts"):
+        if not "type" in config.options(host):
+            raise BaseRemoteBuilderError(f"Host {host} has not 'type' option")
+        if config.get(host, "type") not in ["ssh", "socket"]:
+            raise BaseRemoteBuilderError(f"Host {host}'s type option is invalid, not either: ssh, socket")
+        if not config.get(host, "port"):
+            raise BaseRemoteBuilderError(f"Host {host} has no port specified")
+        if config.get(host, "type") == "ssh":
+            if not config.get(host, "user") and not config.get(host, "keyfile"):
+                raise BaseRemoteBuilderError(f"Host {host} has no user or keyfile specified")
+
+
+def get_config(configfile=None):
+    """Returns the ConfigParser object with the loaded configuration."""
+    _log.debug("Loading the default configuration")
+    config = configparser.ConfigParser()
+    config.read_string(_default_config)
+
+    if configfile:
+        _log.debug(f"Updating the configuration file from {configfile}")
+        config.read(configfile)
+
+    _validate_config(config)
+    return config
+
+
+def _connect(config, host, port=None):
+    """Connect to the host specified in the configuration file using its information.
+
+    In this method we allow to override the 'port' used by the server service.
+    """
+    port = port or config.get(host, "port")
+    connection_type = config.get(host, "type")
+    timeout = config.getint(host, "timeout", fallback=(60 * 60 * 3)) # 3h timeout
+    _log.info(f"Connecting to {host}:{port} using {connection_type} with timeout: {timeout}")
+
+    if connection_type == "socket":
+        conn = rpyc.connect(
+            host=host,
+            port=port,
+            config={
+                "sync_request_timeout": timeout,
+            },
+            keepalive=True,
+        )
+    else:
+        user=config.get(host, "user", fallback=None)
+        keyfile=config.get(host, "keyfile", fallback=None)
+        password=config.get(host, "password", fallback=None)
+        ssh_port=config.getint(host, "ssh_port", fallback=22)
+        _log.debug(f"Connecting via ssh as {user} using the keyfile {keyfile} and or password: {'***' if password else password}")
+        from plumbum import SshMachine
+        rem = SshMachine(
+            host,
+            user=user,
+            keyfile=keyfile,
+            password=password,
+            port=ssh_port,
+            ssh_opts=[
+                "-o UserKnownHostsFile=/dev/null",
+                "-o StrictHostKeyChecking=no",
+            ],
+        )
+        conn = rpyc.ssh_connect(
+            remote_machine=rem,
+            remote_port=port,
+            config={
+                "sync_request_timeout": timeout,
+            },
+        )
+
+    return conn
+
+
+def do_rpmbuild(config, host, conn, args):
     """Build remotly the specified source rpm
 
 
@@ -105,15 +201,7 @@ def do_rpmbuild(conn, args):
         _log.info("  Failed to create container")
         return returncode
 
-    _log.info(f"Connecting to the builder container at: {args.host}:{new_port}")
-    builder = rpyc.connect(
-        host=args.host,
-        port=new_port,
-        config={
-            "sync_request_timeout": 69 * 60 * 3, # 3h timeout
-        },
-        keepalive=True,
-    )
+    builder = _connect(config, host, new_port)
     builder.root.create_workdir()
 
     srpm_filename = os.path.basename(args.source_rpm)
@@ -153,7 +241,7 @@ def do_rpmbuild(conn, args):
             stream.write(builder.root.exposed_retrieve_file(rpm))
 
 
-def do_clean_images(conn, args):
+def do_clean_images(config, host, conn, args):
     """Clean remote_builders related images.
 
     :arg conn: rpyc.core.protocol.Connection object connecting the client to the remote server.
@@ -205,29 +293,32 @@ def main():
     if args.debug:
         _log.setLevel(logging.DEBUG)
 
-    _log.debug(f"Connecting to {args.host}:{args.port}")
-    conn = rpyc.connect(
-        host=args.host,
-        port=args.port,
-        config={
-            "sync_request_timeout": 69 * 60 * 3, # 3h timeout
-        },
-    )
-
-    # Act based on the arguments given
-    return_code = 0
     try:
-        args.func(conn, args)
-    except KeyboardInterrupt:
-        print("\nInterrupted by user.")
-        return_code = 1
-    except exceptions.BaseRemoteBuilderError as err:
-        print(err)
-        return_code = 3
-    except Exception as err:
-        print("Error: {0}".format(err))
-        logging.exception("Generic error catched:")
-        return_code = 2
+        config = get_config(args.config)
+    except BaseRemoteBuilderError as error:
+        print(f"ERROR: {error}")
+        return 5
+
+    return_code = 0
+    for _, host in config.items("hosts"):
+        conn = _connect(config, host)
+
+        # Act based on the arguments given
+        try:
+            args.func(config, host, conn, args)
+        except KeyboardInterrupt:
+            print("\nInterrupted by user.")
+            return_code = 1
+        except exceptions.BaseRemoteBuilderError as err:
+            print(err)
+            return_code = 3
+        except Exception as err:
+            print("Error: {0}".format(err))
+            logging.exception("Generic error catched:")
+            return_code = 2
+
+        if return_code != 0:
+            return return_code
 
     return return_code
 
