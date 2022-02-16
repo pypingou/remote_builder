e04b8ce78dcdac79c6810d3e116c38be3f3ddf4c
Pierre-Yves Chibon <pingou@pingoured.fr>
2022-02-16 17:23:16 +0100

    Drop server/containers.py not needed anymore
    Now that we can define a Containerfile to build or a container image to
    pull for the builder container, we no longer need this file which
    contained our original Containerfile.
    
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- remote_builder/server/containers.py
+++
@@ -1,9 +0,0 @@
-BUILDER_CONTAINER = """
-FROM centos:stream9-development
-
-RUN dnf install -y 'dnf-command(builddep)' rpm-build pip git && \
-    dnf config-manager --set-enabled crb
-RUN pip install rpyc git+https://github.com/pypingou/remote_builder.git
-
-CMD ["remote_builder_server", "--debug"]
-"""
